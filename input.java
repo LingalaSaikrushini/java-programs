cb2e5016867cca344e21bf313e8acb10f7db96e6
LingalaSaikrushini <24WH5A0513@bvrithyderabad.edu.in>
2024-08-22 12:38:07 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,23 @@
+package javaprograms;
+
+import java.util.Scanner;
+
+public class input {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter student details:");
+		System.out.println("enter ypour name:");
+		String s=sc.nextLine();
+		System.out.println("name:"+s);
+		System.out.println("enter your rollno:");
+		int t=sc.nextInt();
+		System.out.println("rollno:"+t);
+		System.out.println("enter your age:");
+		int u=sc.nextInt();
+		System.out.println("age:"+u);
+		
+		
+		
+	}
+
+}
